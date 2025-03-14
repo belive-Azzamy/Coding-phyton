81ad2c2020eec689e6ae411952afa8d99d11c5bc
belive-Azzamy <azzamymuttaqin2@gmail.com>
2025-03-14 17:59:55 +0800

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,12 @@
+print("hello world")
+print("walcome to python programing language")
+
+apple = 5
+jeruk = 10
+total = apple + jeruk
+print(f"{apple} + {jeruk} = {total}")
+
+a = 8
+b = 4
+if a > b:
+    print(f"{a} lebih besar dari {b}")
